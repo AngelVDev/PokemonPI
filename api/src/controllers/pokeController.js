4220c22e231491f74ca13ecc24734231acdf2b48
Chaos <antonioscutari2@hotmail.com>
2022-04-27 22:04:05 -0300

    Still not working but made some progress

--- api/src/controllers/pokeController.js
+++ api/src/controllers/pokeController.js
@@ -2,17 +2,17 @@ const axios = require("axios");
 const { Pokemon, Type } = require("../db");
 
 const pokeApi = async () => {
-  const pokes = [];
-  for (let i = 1; i === 151; i++) {
-    return (pokes = i);
+  let index = 1;
+  let urls = [];
+  while (index <= 151) {
+    urls.push(`https://pokeapi.co/api/v2/pokemon/${index}`);
+    index++;
   }
-  console.log(pokes);
   // const apiUrl = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
-  const apiUrl = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokes}`);
-  const results = apiUrl;
 
-  // const pokeData = [];
-  const pokeData = results.map((poke) => ({
+  Promise.all(urls).then(
+    (poke) => (
+    let pokeData = poke.map({
     id: poke.id,
     name: poke.name,
     types: poke.types.map((t) => t.type.name),
@@ -23,24 +23,7 @@ const pokeApi = async () => {
     speed: poke.stats[5].base_stat,
     weight: poke.weight,
     height: poke.height,
-  }));
-  // for (let i = 0; i < results.length; i++) {
-  //   const secGet = await axios.get(results[i].url);
-  //   const poke = secGet.data;
-
-  //   pokeData.push({
-  // id: poke.id,
-  // name: poke.name,
-  // types: poke.types.map((t) => t.type.name),
-  // image: poke.sprites.other["official-artwork"].front_default,
-  // HP: poke.stats[0].base_stat,
-  // attack: poke.stats[1].base_stat,
-  // defense: poke.stats[2].base_stat,
-  // speed: poke.stats[5].base_stat,
-  // weight: poke.weight,
-  // height: poke.height,
-  //   });
-  // }
+  })))
   return pokeData;
 };
 
