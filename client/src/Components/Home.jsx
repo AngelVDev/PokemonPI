a821d7943b7fd80f82109c72ef05819fea6086e0
Chaos <antonioscutari2@hotmail.com>
2022-05-07 23:51:57 -0300

    [ADD] Pagination, pagination style (temporary). Needs fix

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,51 +1,69 @@
-import React, { useEffect } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { getPokemons, getTypes } from '../Redux/actions';
-import Cards from './Cards'
-import Filters from './Filters';
-import Loader from './Loader'
-import Searchbar from './Searchbar';
+import { getPokemons, getTypes } from "../Redux/actions";
+import Cards from "./Cards";
+import Filters from "./Filters";
+import Loader from "./Loader";
+import Pagination from "./Pagination";
+import Searchbar from "./Searchbar";
 
 const Home = () => {
-    const pokes = useSelector((state) => state.allPokes);
-    // eslint-disable-next-line no-unused-vars
-    const dispatch = useDispatch();
-
-    useEffect(()=>{
-        dispatch(getPokemons());
-        dispatch(getTypes())
-    }, [dispatch])
-if(pokes){
-  return (
-    <div>I'm home
+  const pokes = useSelector((state) => state.allPokes);
+  // eslint-disable-next-line no-unused-vars
+  const dispatch = useDispatch();
+  /*<--Pagination-->*/
+  const [currentPage, setCurrentPage] = useState(1);
+  // eslint-disable-next-line no-unused-vars
+  const [pokesPerPage, setPokesPerPage] = useState(9);
+  const indexOfLastPoke = currentPage * pokesPerPage;
+  const indexOfFirstPoke = indexOfLastPoke - pokesPerPage;
+  // eslint-disable-next-line no-unused-vars
+  const currentPokes = pokes?.slice(indexOfFirstPoke, indexOfLastPoke);
+  const PAGINATION = (pageNum) => {
+    setCurrentPage(pageNum);
+  };
+  /*<--Pagination-->*/
+  useEffect(() => {
+    dispatch(getPokemons());
+    dispatch(getTypes());
+  }, [dispatch]);
+  if (pokes) {
+    return (
+      <div>
+        I'm home
         <nav>
-            <Searchbar/>
-            <Filters/>
-            <button>
-                <Link to={"/create"}>
-                    CREATE POKE
-                </Link>
-            </button>
+          <Searchbar />
+          <Filters />
+          <button>
+            <Link to={"/create"}>CREATE POKE</Link>
+          </button>
         </nav>
         <div>
-            {pokes && pokes?.map((pk)=>{
-                return (
-                    <Link to={'/home/'+ pk.id}>
-                    <Cards
+          {pokes &&
+            pokes?.map((pk) => {
+              return (
+                <Link to={"/home/" + pk.id}>
+                  <Cards
                     name={pk.name}
                     image={pk.image}
                     types={pk.types}
                     key={pk.id}
-                    />
-                    </Link>
-                )
+                  />
+                </Link>
+              );
             })}
         </div>
-    </div>
-  )}else{
-      return(<Loader/>)
+        <Pagination
+          pokesPerPage={pokesPerPage}
+          pokes={pokes.length}
+          pagination={PAGINATION}
+        />
+      </div>
+    );
+  } else {
+    return <Loader />;
   }
-}
+};
 
-export default Home
+export default Home;
---
+++ client/src/Components/Pagination.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Styles/Pagination.css"
+
+export default function Pagination({ pokes, pokesPerPage, pagination }) {
+  const pageNum = [];
+  for (let i = 1; i <= Math.ceil(pokes / pokesPerPage); i++) {
+    pageNum.push(i);
+  }
+  return (
+    <nav id="Pagination">
+      <ul>
+        {pageNum &&
+          pageNum.map((number) => (
+            
+              // eslint-disable-next-line jsx-a11y/anchor-is-valid
+              <a id="Page" onClick={() => pagination(number)}>{number}</a>
+            
+          ))}
+      </ul>
+    </nav>
+  );
+}
