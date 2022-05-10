d14935ca572fb76b55eb232a2f6a61cdac39f2ac
Chaos <antonioscutari2@hotmail.com>
2022-05-10 01:37:49 -0300

    [ADD] Capitalize

--- client/src/Components/Cards.jsx
+++ client/src/Components/Cards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Styles/Cards.css";
 
-function capitalize(string) {
+export function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
