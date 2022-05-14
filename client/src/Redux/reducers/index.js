56df5257f101104b53f83d1e4bc1fdf76987bc57
Chaos <antonioscutari2@hotmail.com>
2022-05-13 22:30:29 -0300

    [NEED FIX]

--- client/src/Redux/reducers/index.js
+++ client/src/Redux/reducers/index.js
@@ -2,7 +2,7 @@ const initialState = {
   pokes: [], //copia burda para hacerle magia
   allPokes: [],
   types: [],
-  pokeDetail: [],
+  pokeDetail: {},
 };
 
 function rootReducer(state = initialState, action) {
@@ -27,6 +27,84 @@ function rootReducer(state = initialState, action) {
       return {
         ...state,
         pokeDetail: action.payload,
+      };
+    case "FILTER_SOURCE":
+      const copy = [...state.pokes];
+      const bySource =
+        action.payload === "MIXED"
+          ? copy
+          : action.payload === "DB"
+          ? state.pokes.filter((el) => el.id.length > 2)
+          : state.allPokes.filter((el) => el.id <= 40);
+      return {
+        ...state,
+        allPokes: bySource,
+      };
+    case "SORT_POKEATK":
+      const falsePoke = [...state.pokes];
+      const sortATK =
+        action.payload === "LOW"
+          ? state.allPokes.sort((a, b) => {
+              if (a.attack > b.attack) {
+                return 1;
+              }
+              if (b.attack > a.attack) {
+                return -1;
+              }
+              return 0;
+            })
+          : action.payload === "HI"
+          ? state.allPokes.sort((a, b) => {
+              if (a.attack > b.attack) {
+                return -1;
+              }
+              if (a.attack > b.attack) {
+                return 1;
+              }
+              return 0;
+            })
+          : falsePoke;
+      return {
+        ...state,
+        allPokes: sortATK,
+      };
+    case "SORT_POKENAME":
+      const sorted =
+        action.payload === "ASC"
+          ? state.pokes.sort((a, b) => {
+              if (a.name > b.name) {
+                return 1;
+              }
+              if (b.name > a.name) {
+                return -1;
+              }
+              return 0;
+            })
+          : action.payload === "DSC"
+          ? state.pokes.sort((a, b) => {
+              if (a.name > b.name) {
+                return -1;
+              }
+              if (a.name > b.name) {
+                return 1;
+              }
+              return 0;
+            })
+          : state.allPokes;
+      return {
+        ...state,
+        allPokes: sorted,
+      };
+    case "FILTER_TYPES":
+      const filteredByType =
+        action.payload === "ALL"
+          ? state.allPokes
+          : state.allPokes.filter((element) =>
+              element.types.includes(action.payload)
+            );
+      return {
+        ...state,
+        allPokes: filteredByType,
       };
     case "POST_POKE":
       return {
